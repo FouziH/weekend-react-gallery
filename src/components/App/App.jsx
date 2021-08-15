127336c93be01e3f2ce211553cab1194c0edff36
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-08-15 11:41:23 -0500

    Added files and images to the project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from '../Header/Header'
+import GalleryList from '../GalleryList/GalleryList';
 
 function App() {
+  
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
+      <Header />
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
       </div>
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,6 @@
+function GalleryList() {
+
+}
+
+
+export default GalleryList
