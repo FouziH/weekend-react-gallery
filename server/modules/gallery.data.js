19e09d496da63391a6fdc80792a29c99b2d93433
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-08-15 11:49:32 -0500

    Added the right path for all of my images'

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -12,9 +12,51 @@ const galleryItems = [
     likes: 0,
   },
   {
-    id: 1,
-    path: "images/goat_small.jpg",
+    id: 3,
+    path: "images/office.jpeg",
+    description: "Photo of a a person working at his work office space",
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: "images/old_man.jpeg",
+    description: "Photo of a an older man",
+    likes: 0,
+  },
+  {
+    id: 5,
+    path: "images/greenPath.jpeg",
+    description: "Photo of a sidewalk.",
+    likes: 0,
+  },
+  {
+    id: 6,
+    path: "images/oceanView.jpeg",
+    description: "ocean view photo.",
+    likes: 0,
+  },
+  {
+    id: 7,
+    path: "images/livingRoom.jpeg",
+    description: "living room photo.",
+    likes: 0,
+  },
+  {
+    id: 8,
+    path: "images/super.jpeg",
+    description: "Goku turning Ultra Instinct Mastered UI",
+    likes: 0,
+  },
+  {
+    id: 9,
+    path: "images/bridge.jpeg",
     description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 10,
+    path: "images/goat_small.jpg",
+    description: "Photo of a bridge.",
     likes: 0,
   },
 ];
