dbcffb1cf5ad33a95b6b76b11c60490e5efda41e
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-08-15 15:08:08 -0500

    Added a conditional rendering regarding the p tage element

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,13 +4,21 @@ function AddLikes() {
 {updateCountLikes(item.id)}
 }
 
+let updateLikes = "";
+if(item.likes  <  1){
+  updateLikes = "No people love this! 🙁"
+}else{
+  updateLikes = `${item.likes} people love this!`
+}
+
+
   console.log("item is", item);
   return (
     <div>
       <img src={item.path} />
       <br />
       <button onClick={AddLikes}>I love this!</button>
-      <p>{item.likes} people love this!</p>
+      <p>{updateLikes}</p>
     </div>
   );
 }
