63117830ec3a50c375428015c4f395926fbed0f1
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-08-16 08:09:37 -0500

    Added comments to my GalleyItem.jsx file

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,26 +1,31 @@
 import { useState } from "react";
-
+//GallryItem function and passing props.item and props.updateCountLikes
 function GalleryItem({ item, updateCountLikes }) {
+  //declaring a useState 
   let [toggle, setToggle] = useState(true)
   
+  //This function is responsible for changing my useState from true to false on clicke
   const toggler =() => {
+    //Setting the toggle true initial state to false 
   setToggle(!toggle)
   }
 
+  //function is responsible on click to updateCount like by passing the item.id
   const AddLikes = () => {
   {updateCountLikes(item.id)}
   }
 
+  //using conditional rendering 
 let updateLikes = "";
 if(item.likes  <  1){
-  updateLikes = "No people love this! 🙁"
+  updateLikes = "No people love this! 🙁"; // if item.like < 1 this code will render 
 }else{
-  updateLikes = `${item.likes} people love this!`
+  updateLikes = `${item.likes} people love this!`; //if item.likes >0 this code will render
 }
-
-  console.log("item is", item);
  
+//using if statement to toggle.
     if (toggle === false) {
+      //if toggle is false, we will return the below code
       return (
         <div>
           <div className="description" onClick={toggler}>{item.description}</div>
@@ -30,7 +35,7 @@ if(item.likes  <  1){
         </div>
       );
     }
-
+//else we will return the following with item.path
     return (
       <div>
         <img onClick={toggler} src={item.path} />
@@ -40,4 +45,4 @@ if(item.likes  <  1){
       </div>
     );
   }
-export default GalleryItem
+export default GalleryItem; //exportig GalleryItem 
